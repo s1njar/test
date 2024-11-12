9ec59192acfe3c8636eedc5c75deba253a27d4ba
s1njar <s1njar.mail@gmail.com>
2024-11-12 21:57:12 +0100

    ok

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 var express = require('express')
 var app = express()
 
+const port = process.env.PORT || 5000;
+
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
-app.listen(3000, function () {
-  console.log('Listening on port 3000...')
+app.listen(port, function () {
+  console.log(`Listening on port ${port}...'`)
 })
